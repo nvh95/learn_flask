96d6afcd01986380dbb60a270a0148a8daafd0c7
nvh95 <nguyenviethunghl00@gmail.com>
2017-11-07 11:48:35 +0700

    Active the form

--- thermos.py
+++ thermos.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect, flash
+from logging import DEBUG
+from datetime import datetime
 
 app = Flask(__name__)
+app.logger.setLevel(DEBUG)
+
+bookmarks = []
+
+
+def store_bookmark(url):
+    bookmarks.append(dict(
+        url=url,
+        user="hung",
+        date=datetime.utcnow()
+    ))
 
 
 @app.route('/')
@@ -9,14 +22,24 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add():
+    if request.method == "POST":
+        url = request.form['url']
+        store_bookmark(url)
+        app.logger.debug('stored url: ' + url)
+        return redirect(url_for('index'))
     return render_template('add.html')
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template()
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
